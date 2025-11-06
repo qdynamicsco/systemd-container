b2214b984c45c4c6007fa008b5d485405d5bdfdc
Daniel Ward <735837+DanWard@users.noreply.github.com>
2025-11-07 07:57:50 +1100

    disable xserver sleep

--- start.sh
+++ start.sh
@@ -10,7 +10,9 @@ mkdir -p -m 0700 /tmp/xdg
 dbus-daemon --system
 
 # Start the X server and the Openbox session
-xinit
+# -s 0: disables screensaver timeout
+# -dpms: disables Display Power Management Signaling
+xinit -- /usr/bin/X :0 -nocursor -s 0 -dpms
 
 # Fallback to keep the container running if xinit exits
 tail -f /dev/null
